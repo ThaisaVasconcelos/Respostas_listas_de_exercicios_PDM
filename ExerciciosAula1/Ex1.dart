134e574139d347fe72bef04a320e078e417d1ff6
Thaísa Vasconcelos <thaisasvasconcelos@gmail.com>
2025-03-18 11:20:06 -0300

    Update Ex1.dart

---
+++ ExerciciosAula1/Ex1.dart
@@ -0,0 +1,24 @@
+//Criar uma classe Laptop com as propriedades [id, nomde, ram] e croar 3 objetos dela, imprimindo todos os detalhes
+
+class Laptop {
+  int id;
+  String nome;
+  int ram;
+
+  Laptop(this.id, this.nome, this.ram);
+
+  @override
+  String toString() {
+    return "Laptop: $id, $nome, $ram";
+  }
+}
+
+void main() {
+  Laptop laptop1 = Laptop(1, "Dell", 8);
+  Laptop laptop2 = Laptop(2, "HP", 16);
+  Laptop laptop3 = Laptop(3, "Lenovo", 32);
+
+  print(laptop1); //Laptop: 1, Dell, 8
+  print(laptop2); //Laptop: 2, HP, 16
+  print(laptop3); //Laptop: 3, Lenovo, 32
+}
